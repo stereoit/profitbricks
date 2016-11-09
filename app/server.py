af4270d91efa76acb62a1ed841559dd688686e9e
Robert Smol <robert.smol@stereoit.com>
2016-11-09 10:28:34 +0100

    Add CORS headers to allow other domain serving

--- app/server.py
+++ app/server.py
@@ -13,5 +13,15 @@ from api import api_manager
 
 manager = Manager(app)
 
+def add_cors_header(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'HEAD, GET, POST, PATCH, PUT, OPTIONS, DELETE'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+
+    return response
+
+app.after_request(add_cors_header)
+
 if __name__ == '__main__':
 	manager.run()
