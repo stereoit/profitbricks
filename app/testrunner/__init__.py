2a281eddd6d0a2826b89a40ff2038736483924cf
Robert Smol <robert.smol@stereoit.com>
2016-11-09 23:23:03 +0100

    Added dummy celery tasks for simulating testrunners, exposed them over API at /api/testrunner/:<id>

--- app/testrunner/__init__.py
+++ app/testrunner/__init__.py
@@ -1,8 +1,20 @@
+import encodings.idna
+import random
+import time
 from celery import Celery
 from celery.signals import worker_init
+from app import app
 
-celery = Celery('tasks', backend='rpc://', broker='amqp://guest@localhost')
+celery = Celery(
+    app.name,
+    broker=app.config['CELERY_BROKER_URL'],
+    result_persistent=True
+)
+celery.conf.update(app.config) # this takes care of `backend`
 
+
+
+#tst value for init_connect signal
 db_conn = None
 
 # I might replace this with the app context
@@ -14,9 +26,32 @@ db_conn = None
 
 
 # version with app context
+# take testrun_id, read it's data, execute and store back results
 @celery.task
 def test_runner(testrun_id):
     """Background task to proces given Testrun."""
     with app.app_context():
         print("Processing ", testrun_id)
         return True
+
+
+#I can used this as a sample pytest
+@celery.task(bind=True)
+def long_task(self):
+    """Background task that runs a long function with progress reports."""
+    verb = ['Starting up', 'Booting', 'Repairing', 'Loading', 'Checking']
+    adjective = ['master', 'radiant', 'silent', 'harmonic', 'fast']
+    noun = ['solar array', 'particle reshaper', 'cosmic ray', 'orbiter', 'bit']
+    message = ''
+    total = random.randint(10, 50)
+    for i in range(total):
+        if not message or random.random() < 0.25:
+            message = '{0} {1} {2}...'.format(random.choice(verb),
+                                              random.choice(adjective),
+                                              random.choice(noun))
+        self.update_state(state='PROGRESS',
+                          meta={'current': i, 'total': total,
+                                'status': message})
+        time.sleep(1)
+    return {'current': 100, 'total': 100, 'status': 'Task completed!',
+            'result': 42}
---
+++ app/testrunner/api.py
@@ -0,0 +1,34 @@
+#from __main__ import app
+from app import app
+from . import long_task
+from flask import jsonify
+
+@app.route('/api/testrunner/<task_id>')
+def taskstatus(task_id):
+    task = long_task.AsyncResult(task_id)
+    if task.state == 'PENDING':
+        # job did not start yet
+        response = {
+            'state': task.state,
+            'current': 0,
+            'total': 1,
+            'status': 'Pending...'
+        }
+    elif task.state != 'FAILURE':
+        response = {
+            'state': task.state,
+            'current': task.info.get('current', 0),
+            'total': task.info.get('total', 1),
+            'status': task.info.get('status', '')
+        }
+        if 'result' in task.info:
+            response['result'] = task.info['result']
+    else:
+        # something went wrong in the background job
+        response = {
+            'state': task.state,
+            'current': 1,
+            'total': 1,
+            'status': str(task.info),  # this is the exception raised
+        }
+    return jsonify(response)
