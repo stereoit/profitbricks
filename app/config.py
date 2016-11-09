b846275fadba8bd4b7e168a37cc5ba4ba4612f7f
Robert Smol <robert.smol@stereoit.com>
2016-11-09 23:19:42 +0100

    Updated app.config for CELERY backends and surpassed SQLALCHEMY message

--- app/config.py
+++ app/config.py
@@ -2,5 +2,14 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# Database connectoin, to me it looks like I could do with Redis complely
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+# Celery configuration
+# RabbitMQ for message queue and
+# Redis for storing results
+CELERY_BROKER_URL = 'amqp://guest@localhost'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
