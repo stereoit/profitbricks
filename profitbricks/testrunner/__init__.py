20c04d3c0c16634e011997a9cd500f95c1d8c251
Robert Smol <robert.smol@stereoit.com>
2016-11-14 10:59:02 +0100

    removed dummy code from testrunner

--- profitbricks/testrunner/__init__.py
+++ profitbricks/testrunner/__init__.py
@@ -88,57 +88,6 @@ def test_runner(self, testrun_id):
     return {'status': testrun.status}
 
 
-
-# take testrun_id, read it's data, execute and store back results
-# @celery.task(bind=True)
-# def test_runner(self, testrun_id):
-#     """
-#     Background task to proces given Testrun.
-#
-#     It will connect to DB, read Testrun data and download files
-#     Process this files.
-#     Store back the results/exception
-#
-#     For simplicity yet I am returning simple value.
-#     """
-#     testrun = TestRun.query.get(testrun_id)
-#
-#     if testrun is None:
-#         return { 'status': 'Failed', 'error': "TestRun not found"}
-#
-#     testrun.status = 'RUNNIG'
-#     db.session.add(testrun)
-#     db.session.commit()
-#
-#     verb = ['Starting up', 'Booting', 'Repairing', 'Loading', 'Checking']
-#     adjective = ['master', 'radiant', 'silent', 'harmonic', 'fast']
-#     noun = ['solar array', 'particle reshaper', 'cosmic ray', 'orbiter', 'bit']
-#     message = ''
-#     total = random.randint(10, 50)
-#     for i in range(total):
-#         if not message or random.random() < 0.25:
-#             message = '{0} {1} {2}...'.format(random.choice(verb),
-#                                               random.choice(adjective),
-#                                               random.choice(noun))
-#         self.update_state(
-#             state='PROGRESS',
-#             meta={
-#                 'testrun_id': testrun_id,
-#                 'current': i,
-#                 'total': total,
-#                 'status': message
-#             },
-#         )
-#         time.sleep(1)
-#
-#     #update the state of the TestRun
-#     testrun.status = 'SUCCESS'
-#     db.session.add(testrun)
-#     db.session.commit()
-#
-#     return {'current': 100, 'total': 100, 'status': 'Task completed!', 'result': 42}
-
-
 def start_testrunner(testrun_id):
 	'''
 	Start new testrunner, returls async id
